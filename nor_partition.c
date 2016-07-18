341ebba2d5bc49e3bf272adae6b0ff9181e56737
Jarek Malec <dzardacz@gmail.com>
2016-07-18 22:51:20 +0200

    Added files.

---
+++ nor_partition.c
@@ -0,0 +1,9 @@
+#include "partition.h"
+
+status_code_t partition_read(const struct partition *part, uint32_t addr, unsigned char *dest, int32_t bytes)
+{
+}
+
+status_code_t partition_write(const struct partition *part, uint32_t addr, unsigned char *src, int32_t bytes)
+{
+}
---
+++ partition.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum status_code {
+	STATUS_OK               =  0, //!< Success
+	STATUS_ERR_BUSY         =  0x19,
+	STATUS_ERR_DENIED       =  0x1C,
+	STATUS_ERR_TIMEOUT      =  0x12,
+	ERR_IO_ERROR            =  -1, //!< I/O error
+	ERR_FLUSHED             =  -2, //!< Request flushed from queue
+	ERR_TIMEOUT             =  -3, //!< Operation timed out
+	ERR_BAD_DATA            =  -4, //!< Data integrity check failed
+	ERR_PROTOCOL            =  -5, //!< Protocol error
+	ERR_UNSUPPORTED_DEV     =  -6, //!< Unsupported device
+	ERR_NO_MEMORY           =  -7, //!< Insufficient memory
+	ERR_INVALID_ARG         =  -8, //!< Invalid argument
+	ERR_BAD_ADDRESS         =  -9, //!< Bad address
+	ERR_BUSY                =  -10, //!< Resource is busy
+	ERR_BAD_FORMAT          =  -11, //!< Data format not recognized
+	ERR_NO_TIMER            =  -12, //!< No timer available
+	ERR_TIMER_ALREADY_RUNNING   =  -13, //!< Timer already running
+	ERR_TIMER_NOT_RUNNING   =  -14, //!< Timer not running
+	ERR_ABORTED             =  -15, //!< Operation aborted by user
+} status_code_t;
+
+//typedef status_code status_code_t;
+
+struct partition {
+	uint32_t start;
+	uint32_t size;
+};
+
+#define DECLARE_PARTITION(name, start, size) \
+struct partition name = { \
+	.start = start, \
+	.size = size \
+}
+
+status_code_t partition_read(const struct partition *part, uint32_t addr, unsigned char *dest, int32_t bytes);
+status_code_t partition_write(const struct partition *part, uint32_t addr, unsigned char *src, int32_t bytes);
