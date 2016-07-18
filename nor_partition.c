dfc1714b76c50d75597c0e6e17fe7b0cbc9bd6a0
Jarek Malec <dzardacz@gmail.com>
2016-07-18 23:51:12 +0200

    Added first test

--- nor_partition.c
+++ nor_partition.c
@@ -1,9 +1,19 @@
 #include "partition.h"
 
-status_code_t partition_read(const struct partition *part, uint32_t addr, unsigned char *dest, int32_t bytes)
+#define check_in_range(part, addr, ptr, bytes) \
+	(addr + bytes <= part->start + part->size)
+
+
+status_code_t partition_read(const partition_t *part, uint32_t addr, unsigned char *dest, uint32_t bytes)
 {
+	if (!check_in_range(part, addr, dest, bytes)) {
+		return ERR_BAD_ADDRESS;
+	}
 }
 
-status_code_t partition_write(const struct partition *part, uint32_t addr, unsigned char *src, int32_t bytes)
+status_code_t partition_write(const partition_t *part, uint32_t addr, unsigned char *src, uint32_t bytes)
 {
+	if (!check_in_range(part, addr, src, bytes)) {
+		return ERR_BAD_ADDRESS;
+	}
 }
--- partition.h
+++ partition.h
@@ -26,16 +26,16 @@ typedef enum status_code {
 
 //typedef status_code status_code_t;
 
-struct partition {
+typedef struct {
 	uint32_t start;
 	uint32_t size;
-};
+} partition_t;
 
-#define DECLARE_PARTITION(name, start, size) \
-struct partition name = { \
-	.start = start, \
-	.size = size \
+#define DECLARE_PARTITION(NAME, START, SIZE) \
+partition_t NAME = { \
+	.start = START, \
+	.size = SIZE \
 }
 
-status_code_t partition_read(const struct partition *part, uint32_t addr, unsigned char *dest, int32_t bytes);
-status_code_t partition_write(const struct partition *part, uint32_t addr, unsigned char *src, int32_t bytes);
+status_code_t partition_read(const partition_t *part, uint32_t addr, unsigned char *dest, uint32_t bytes);
+status_code_t partition_write(const partition_t *part, uint32_t addr, unsigned char *src, uint32_t bytes);
